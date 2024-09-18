5279c068d8882df77dd984732356c2cc98315f42
Juan-CCC <20210654@uthh.edu.mx>
2024-09-18 10:37:18 -0600

    first commit

---
+++ src/feature/footer_Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer_Component = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default footer_Component
---
+++ src/feature/header_Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header_Component = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default header_Component
